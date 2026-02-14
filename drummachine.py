d8bb19782f3b82347e7c7453306bc20efd2d769f
Iulius Curt <iulius.curt@gmail.com>
2026-02-14 13:57:31 +0200

    tweak

--- drummachine.py
+++ drummachine.py
@@ -488,7 +488,7 @@ class OLEDHandler:
                 # Try FreeSans Bold first (similar to logisoso)
                 self.font_large = ImageFont.truetype("./fonts/SpaceMono-Bold.ttf", 32)
                 self.font_medium = ImageFont.truetype("./fonts/SpaceMono-Bold.ttf", 20)
-                self.font_small = ImageFont.truetype("./fonts/m42.TTF", 15)
+                self.font_small = ImageFont.truetype("./fonts/m42.TTF", 12)
             except:
                 try:
                     # Fallback to DejaVu Sans Bold
