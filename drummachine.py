6a2fa074dc2d36b32bd59a2976628d9a292e09fe
Iulius Curt <iulius.curt@gmail.com>
2026-02-14 11:50:23 +0200

    smaller font

--- drummachine.py
+++ drummachine.py
@@ -466,7 +466,7 @@ class OLEDHandler:
             # Try to load fonts - aim for bold, chunky look like Arduino u8g2
             try:
                 # Try FreeSans Bold first (similar to logisoso)
-                self.font_large = ImageFont.truetype("./fonts/SpaceMono-Bold.ttf", 34)
+                self.font_large = ImageFont.truetype("./fonts/SpaceMono-Bold.ttf", 32)
             except:
                 try:
                     # Fallback to DejaVu Sans Bold
@@ -548,9 +548,9 @@ class OLEDHandler:
                 # Draw text with anchor='ra' (right-aligned)
                 self.draw.text((120, 0), vol_text, font=self.font_large, fill=255, anchor='ra')
                 # Draw Volume Bar
-                self.draw.rectangle((value_xoffset, 45, 120, 44), outline=1)
+                self.draw.rectangle((value_xoffset, 40, 120, 44), outline=1)
                 fill_width = int(58 * self.sequencer.volume)
-                self.draw.rectangle((value_xoffset, 45, value_xoffset + fill_width, 44), fill=1)
+                self.draw.rectangle((value_xoffset, 40, value_xoffset + fill_width, 44), fill=1)
 
             elif self.sequencer.mode == 'DIST':
                 self.image.paste(self.icon_fx, (5, 6))
@@ -559,9 +559,9 @@ class OLEDHandler:
                 # Draw text with anchor='ra' (right-aligned)
                 self.draw.text((120, 0), dist_text, font=self.font_large, fill=255, anchor='ra')
                 # Draw Distortion Bar
-                self.draw.rectangle((value_xoffset, 45, 120, 44), outline=1)
+                self.draw.rectangle((value_xoffset, 40, 120, 44), outline=1)
                 fill_width = int(58 * self.sequencer.distortion)
-                self.draw.rectangle((value_xoffset, 45, value_xoffset + fill_width, 44), fill=1)
+                self.draw.rectangle((value_xoffset, 40, value_xoffset + fill_width, 44), fill=1)
 
             self.device.display(self.image)
             
