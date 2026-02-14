2db34ac59aa060439e137da1449f716d4c15dd42
Iulius Curt <iulius.curt@gmail.com>
2026-02-14 14:04:02 +0200

    trying to space out letters

--- drummachine.py
+++ drummachine.py
@@ -488,7 +488,7 @@ class OLEDHandler:
                 # Try FreeSans Bold first (similar to logisoso)
                 self.font_large = ImageFont.truetype("./fonts/SpaceMono-Bold.ttf", 32)
                 self.font_medium = ImageFont.truetype("./fonts/SpaceMono-Bold.ttf", 20)
-                self.font_small = ImageFont.truetype("./fonts/m42.TTF", 7)
+                self.font_small = ImageFont.truetype("./fonts/m42.TTF", 8)
             except:
                 try:
                     # Fallback to DejaVu Sans Bold
@@ -563,7 +563,7 @@ class OLEDHandler:
                 self.device.display(self.image)
                 return
 
-            self.draw.text((5, 47), f"{self.MODES[self.sequencer.mode]}", font=self.font_small, fill=255)
+            self.draw.text((5, 47), " ".join(f"{self.MODES[self.sequencer.mode]}"), font=self.font_small, fill=255)
 
             # Mode Specifics
             if self.sequencer.mode == 'BPM':
